df40c9905ce0d8ecb78daf1d2c4b4aa14eda0e0a
cnjimbo <happycoder@foxmail.com>
2022-05-09 14:53:22 +0800

    fix test error

--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const wait = require('./wait')
+const process = require('process')
+const path = require('path')
+const exec = require('@actions/exec')
 
 test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
+  await expect(wait('foo')).rejects.toThrow('milliseconds not a number')
+})
 
 test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
+  const start = new Date()
+  await wait(500)
+  const end = new Date()
+  var delta = Math.abs(end - start)
+  expect(delta).toBeGreaterThanOrEqual(500)
+})
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node "${ip}"`, { env: process.env }).toString();
-  console.log(result);
+  process.env['INPUT_MILLISECONDS'] = 100
+  const ip = path.join(__dirname, 'index.js')
+  const result = exec.exec('node', [ip, process.env])
+  console.log(result)
 })
